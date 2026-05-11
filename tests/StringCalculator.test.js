3cbce7e89ec91f026f5a7802ca915f5c0ced7dd4
AlexisMerce <alexis.am.2001@gmail.com>
2026-05-11 20:52:32 -0300

    se realizo el segunndo test

--- tests/StringCalculator.test.js
+++ tests/StringCalculator.test.js
@@ -4,4 +4,10 @@ describe('StringCalculator', () => {
   test('add("") returns 0', () => {
     expect(add("")).toBe(0);
   });
+
+  test('add("1") returns 1', () => {
+  expect(add("1")).toBe(1);
 });
+
+});
+
