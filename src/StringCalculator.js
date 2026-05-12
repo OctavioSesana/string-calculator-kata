1490b8ad0f7787aea62e8761aef1cfc12febf307
OctavioSesana <osesana@frro.utn.edu.ar>
2026-05-11 21:11:44 -0300

    nueva linea como separador

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,6 +1,6 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  return numbers.split(",").reduce((sum, n) => sum + parseInt(n), 0);
+  return numbers.split(/,|\n/).reduce((sum, n) => sum + parseInt(n), 0);
 }
 
 module.exports = { add };
